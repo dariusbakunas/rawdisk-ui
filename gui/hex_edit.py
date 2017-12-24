99ae62cad39dca19e482bf2852924284392f1214
Darius Bakunas-Milanowski <bakunas@gmail.com>
2017-12-24 14:20:14 -0500

    add simple address section

--- gui/hex_edit.py
+++ gui/hex_edit.py
@@ -1,10 +1,13 @@
 # coding=utf-8
 
 import math
+import logging
 from util.mem_buffer import MemBuffer
 from PyQt5.QtWidgets import QAbstractScrollArea
 from PyQt5.QtGui import QPainter, QColor, QFontDatabase, \
     QFontMetrics
+
+logger = logging.getLogger(__name__)
 
 class HexEdit(QAbstractScrollArea):
     def __init__(self, parent = None):
@@ -12,6 +15,7 @@ class HexEdit(QAbstractScrollArea):
         self.__buffer = None
         self.__rows_shown = 0
         self.__total_rows = 0
+        self.__addr_width = 0
         self.initUI()
 
     def initUI(self):
@@ -55,9 +59,14 @@ class HexEdit(QAbstractScrollArea):
             self.__buffer.close()
 
     def load(self, filename):
-        self.__buffer = MemBuffer(filename, min_size=self.__rows_shown * self.__bytes_per_row)
-        self.verticalScrollBar().setValue(0)
+        self.__buffer = MemBuffer(
+            filename,
+            min_size=self.__rows_shown * self.__bytes_per_row)
 
+        self.__addr_width = self.__font_metrics.width(
+            ' {:X}  '.format(self.__buffer.size))
+
+        self.verticalScrollBar().setValue(0)
         self.update_total_rows()
         self.viewport().update()
 
@@ -81,7 +90,7 @@ class HexEdit(QAbstractScrollArea):
         return num_bytes - 1 if width > row_width else num_bytes
 
     def update_metrics(self):
-        viewport_width = self.viewport().width()
+        viewport_width = self.viewport().width() - self.__addr_width
         viewport_height = self.viewport().height()
         self.__bytes_per_row = self.get_bytes_per_row(
             font_metrics=self.__font_metrics,
@@ -103,10 +112,13 @@ class HexEdit(QAbstractScrollArea):
 
         if self.__buffer:
             remaining_rows = math.ceil(self.__buffer.remaining_bytes / self.__bytes_per_row)
+            padding = len('{:X}'.format(self.__buffer.size))
 
             for row in range(min(int(self.__rows_shown), remaining_rows)):
                 y_pos = (row + 1) * self.__font_metrics.height()
                 offset = row * self.__bytes_per_row + self.__buffer.offset
                 row_bytes = self.__buffer[offset:offset + self.__bytes_per_row]
-                str = ''.join('{:02x} '.format(x) for x in row_bytes)
-                qp.drawText(0, y_pos, str)
+                addr_str = '{:X}'.format(offset)
+                bytes_str = ''.join('{:02X} '.format(x) for x in row_bytes)
+                qp.drawText(0, y_pos, ' ' + addr_str.rjust(padding, '0'))
+                qp.drawText(self.__addr_width, y_pos, bytes_str)
